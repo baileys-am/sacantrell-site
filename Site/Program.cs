090ea8490467b6d67fec6bdb423f8f1b14cf98ba
Steven Cantrell <astevencantrell@gmail.com>
2018-07-31 22:41:56 -0400

    host only on localhost

--- Site/Program.cs
+++ Site/Program.cs
@@ -19,7 +19,7 @@ namespace Sacantrell.Site
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:5000")
+                .UseUrls("http://127.0.0.1:5000")
                 .UseStartup<Startup>();
     }
 }
